99a968413768e8351916b124e88e7c98bd7104ef
EJisAwaken <fenoasy@yahoo.com>
2023-11-11 14:28:00 +0300

    suite

---
+++ Developpement/pages/menuGauche.php
@@ -0,0 +1,29 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="./pages/planete.php" class="nav-link">
+                <p>Planete</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./pages/astronaute.php" class="nav-link">
+                <p>Astronaute</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./pages/missions.php" class="nav-link">
+                <p>Mission</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./pages/vaisseau.php" class="nav-link">
+                <p>Vaisseau</p>
+            </a>
+        </li>
+    </ul>
+
+
+</nav>
+<!-- /.sidebar-menu -->
